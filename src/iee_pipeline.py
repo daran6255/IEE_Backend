60723e7f11434f5ce30b037c338752d14b8d6011
DESKTOP-2TEJ1C0\WVF-DL-90 <darandharani6255@gmail.com>
2024-03-05 17:23:42 +0530

    IEE pipeline init

---
+++ src/iee_pipeline.py
@@ -0,0 +1,23 @@
+
+
+class IEEPipeline:
+    image_enhancer = ImageEnhancer()
+    
+    def __init__(self):
+        pass
+    
+    def image_preprocessing(self, img_path):
+        
+        return "/file-enhanced.jpg"
+    
+    def extract_text(self, img_path):
+        extracted_text = "kjhfakhfkaf kjahfkasjf"
+        return extracted_text
+    
+    def text_preprocessing(self, text):
+        
+        pass
+    
+    def extract_entities(self, text):
+        
+        return {"invioce_no": 1233}
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, send_file
+from random import random
+from flask import Flask, jsonify, render_template, request, send_file
 import json
 import pandas as pd
 import os
 
+from iee_pipeline import IEEPipeline
+
 # template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 app = Flask(__name__, static_folder='frontend/static', template_folder='frontend/templates')
 
+iee_pipeline = IEEPipeline()
+
+TEMP_DIR = 'temp'
+if not os.path.exists(TEMP_DIR):
+    os.makedirs(TEMP_DIR)
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -39,7 +48,20 @@ def download_json():
 
     return send_file(json_file, as_attachment=True)
 
-# @app.route('/extract_data')
-
+@app.route('/process_invoice', methods=['POST'])
+def process_invoice():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'})
+    file = request.files['file']
+    file_path = os.path.join(TEMP_DIR, 'temp_file_' + random())
+    file.save(file_path)
+    
+    pp_output = iee_pipeline.image_preprocessing(file_path)
+    ocr_output = iee_pipeline.extract_text(pp_output)
+    txt_pp_ouput = iee_pipeline.text_preprocessing(ocr_output)
+    entities_extracted = iee_pipeline.extract_entities(txt_pp_ouput)
+    
+    return jsonify(entities_extracted)
+    
 if __name__ == '__main__':
     app.run(debug=True)
