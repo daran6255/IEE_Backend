0073b7e67a3e20ab7fb2327e5f06494e2c2c4674
DESKTOP-2TEJ1C0\WVF-DL-90 <darandharani6255@gmail.com>
2024-03-14 10:25:45 +0530

    Project refactored

--- src/export_model.py
+++
@@ -1,7 +0,0 @@
-import spacy
-
-
-model_path =r'C:\Users\WVF-DL-90\Desktop\Invoice_Entities_Extraction\ner\output\model-best'
-nlp = spacy.load(model_path)
-output_dir = r'data\spacy_model'
-nlp.to_disk(output_dir)
